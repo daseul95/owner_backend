860cb4e6cf9a8d2049ad3ff14cd6c653b0655cab
Kim Daseul <osl123o@naver.com>
2025-08-07 21:46:17 +0900

    가게 등록,조회,수정,삭제 가능

---
+++ src/main/generated/me/dev/entity/QStore.java
@@ -0,0 +1,75 @@
+package me.dev.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QStore is a Querydsl query type for Store
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QStore extends EntityPathBase<Store> {
+
+    private static final long serialVersionUID = 281447877L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QStore store = new QStore("store");
+
+    public final StringPath address = createString("address");
+
+    public final StringPath businessNum = createString("businessNum");
+
+    public final DateTimePath<java.sql.Timestamp> created_at = createDateTime("created_at", java.sql.Timestamp.class);
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath image = createString("image");
+
+    public final NumberPath<Float> lat = createNumber("lat", Float.class);
+
+    public final NumberPath<Float> longti = createNumber("longti", Float.class);
+
+    public final ListPath<Menu, QMenu> menus = this.<Menu, QMenu>createList("menus", Menu.class, QMenu.class, PathInits.DIRECT2);
+
+    public final QUser owner;
+
+    public final StringPath phone = createString("phone");
+
+    public final StringPath postNum = createString("postNum");
+
+    public final StringPath storeName = createString("storeName");
+
+    public final DateTimePath<java.sql.Timestamp> updated_at = createDateTime("updated_at", java.sql.Timestamp.class);
+
+    public QStore(String variable) {
+        this(Store.class, forVariable(variable), INITS);
+    }
+
+    public QStore(Path<? extends Store> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QStore(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QStore(PathMetadata metadata, PathInits inits) {
+        this(Store.class, metadata, inits);
+    }
+
+    public QStore(Class<? extends Store> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.owner = inits.isInitialized("owner") ? new QUser(forProperty("owner")) : null;
+    }
+
+}
+
